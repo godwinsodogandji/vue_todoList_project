02acc61be5ae718e1cfbddedda951476c6383b87
godwinsodogandji <jeansodogandji@gmail.com>
2024-10-05 18:25:38 +0100

    init

---
+++ src/components/TaskList.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container mx-auto p-4 pt-6 md:p-6 lg:p-12">
+      <h1 class="text-rose-300">Hello world</h1>
+    </div>
+  </template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
